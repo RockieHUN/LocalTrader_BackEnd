54136f8a6ba6b3698dfd823412b3666cf1c3129d
Rockie <csaszar.akos99@gmail.com>
2021-04-06 17:45:18 +0300

    algolia functions

--- functions/index.js
+++ functions/index.js
@@ -8,12 +8,20 @@ const Serializer = require("./Models/Serializer");
 const functions = require("firebase-functions");
 const admin = require ("firebase-admin");
 const serviceAccount = require('./ServiceAccountKey.json');
+const algoliasearch = require("algoliasearch");
 
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
   });
 
+    
+    const ALGOLIA_APP_ID = functions.config().algolia.app_id;
+    const ALGOLIA_ADMIN_KEY = functions.config().algolia.admin_api_key;
+    const ALGOLIA_SEARCH_KEY = functions.config().algolia.search_api_key;
+
+    const ALGOLIA_BUSINESS_INDEX_NAME = 'businesses';
+    const client = algoliasearch(ALGOLIA_APP_ID,ALGOLIA_ADMIN_KEY);
 
   exports.createQuadTree = functions.https.onRequest( (request, response ) => {
 
@@ -70,9 +78,7 @@ admin.initializeApp({
         
   });
 
-  
-
-  exports.getLocalBusinesses = functions.https.onRequest( (request, response)=>{
+  exports.getLocalBusinesses = functions.https.onRequest( (request, ressponse)=>{
 
     const longitude = request.body.longitude;
     const latitude = request.body.latitude;
@@ -161,8 +167,69 @@ admin.initializeApp({
     }
 
     });
+
+    //------------------------ ALGOLIA ---------------------------------
+    
+   exports.onBusinessCreated = functions.firestore.document('businesses/{businessId}')
+        .onCreate( (snapshot, context) =>{
+            const business = snapshot.data();
+
+            const newObject = {
+                "objectID" : context.params.businessId,
+                "businessName" : business.name
+            };
+
+            business.objectID = context.params.businessId;
+
+            const index = client.initIndex(ALGOLIA_BUSINESS_INDEX_NAME);
+            index.saveObject(newObject);
+            return;
+        });
+
+    exports.search = functions.https.onRequest( ( request, response) =>{
+
+        const searchTerm = request.body.searchTerm;
+
+        if (searchTerm == null || searchTerm == undefined){
+            response
+            .status(200)
+            .type('application/json')
+            .send([]);
+        }
+
+
+        const index = client.initIndex(ALGOLIA_BUSINESS_INDEX_NAME);
+        
+        index.search(searchTerm,{
+            attributesToRetrieve: ['businessName','objectID'],
+            hitsPerPage : 10
+        })
+            .then( function (responses){
+                const hits = responses.hits;
+
+                let list = [];
+                hits.forEach(hit => {
+                    list.push({
+                        "businessName" : hit.businessName,
+                        "businessId" :hit.objectID
+                    });
+                })
+
+                response
+                .status(200)
+                    .type('application/json')
+                    .send(JSON.stringify(list));
+            })
+            .catch(e =>{
+                response
+                .status(200)
+                .type('application/json')
+                .send([]);
+            });
+    });
     
 
+    //------------------ NOTIFICATIONS -------------------------
 
   exports.sendNotification = functions.firestore.document("orderRequests/{requestId}")
         .onCreate((snapshot, context) => {
